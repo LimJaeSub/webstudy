ae4066e6ea3e1d4ba7a5d15e844566e7cd7ea552
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-08-16 13:03:28 +0900

    ddd
    ddd

---
+++ jasub/selfjavascript/3_3_inputoutput.js
@@ -0,0 +1,31 @@
+//ch03_14
+// var x = "함수를 이용한 메시지 출력"
+// alert(x);
+
+//ch03_15
+// var name = "hello";
+// var age = 15;
+// document.write("name="+name+"<br>");
+// document.write("age="+age);
+
+//ch03_18
+// var str01 = "first";
+// var str02 = "second";
+// console.log(str01);
+// console.log(str02);
+// console.log(str01+str02);
+
+// //ch03_19
+// var bool=confirm("yes or no");
+// document.write(bool);
+// //확인을 누르면 true반환 취소를 누르면 false반환
+
+
+// //-----------------입력문---------------------
+// //ch03_20
+// var age=prompt("나이를 입력하세요","숫자를 입력하세요");
+// document.write(age);
+// // prompt(입력칸에 나오는 글자 , 정보창에 나오는글자)
+// // 사용자가 입력칸에 숫자를 입력하면 age에 숫자가 입력된다.
+// // 13을 입력했을경우 age==13
+
