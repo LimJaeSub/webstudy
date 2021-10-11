759e197815a4c43cdf8ff03b5a1d17e30c51d245
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-10-12 01:27:57 +0900

    dd
    ddd

---
+++ jasub/selfjavascript/nomard/test.js
@@ -0,0 +1,10 @@
+const title = document.querySelector(".hello:first-child h1")
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
+
+
+title.addEventListener("click",handleTitleClick)
+s
+handleTitleClick()
