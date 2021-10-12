7add3baa37731df171bbba9d072148deeea57b1c
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-10-13 02:28:04 +0900

    dom2
    dom2

--- jasub/selfjavascript/nomard/test.js
+++ jasub/selfjavascript/nomard/test.js
@@ -1,10 +1,23 @@
 const title = document.querySelector(".hello:first-child h1")
+const title2 = document.querySelectorAll(".hello")
 
 function handleTitleClick(){
-    console.log("title was clicked!");
+    alert("hello");
+}
+function mouseenterfun(){
+    alert("mouse is here!");
+}
+
+for(var a=0;a<3;a++)
+{
+    console.log(title2[a]);
 }
 
 
-title.addEventListener("click",handleTitleClick)
-s
-handleTitleClick()
+// title2.addEventListener("click",handleTitleClick)
+
+
+function handleWindowResize{
+    document.body.style.backgroundColor="tomato";
+}
+window.addEventListener("resize",handleWindowResize)
