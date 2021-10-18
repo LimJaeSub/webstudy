1aaf69f0d49355e36851c42e897df1cd6d6a564f
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-10-19 02:07:55 +0900

    dfsdf
    sdfsd

--- jasub/selfjavascript/nomard/test.js
+++ jasub/selfjavascript/nomard/test.js
@@ -1,21 +1,38 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
+
+// function titleClick(){
+//     if(title.style.color == "blue")
+//     {
+//         title.style.color = "tomato";
+//     }
+//     else if(title.style.color == "tomato")
+//     {
+//         title.style.color="blue";
+//     }
+//     else{
+//         title.style.color="blue";
+//     }
+// }
+
+
+
+// function titleClick(){
+//     const clickedclass = "active";
+//     if(h1.classList.contains(clickedclass)){
+//         h1.classList.remove(clickedclass);
+//     }
+//     else{
+//         h1.classList.add(clickedclass);
+//     }
+// }
 
 function titleClick(){
-    if(title.style.color == "blue")
-    {
-        title.style.color = "tomato";
-    }
-    else if(title.style.color == "tomato")
-    {
-        title.style.color="blue";
-    }
-    else{
-        title.style.color="blue";
-    }
+    const clickedClass = "active";
+    h1.classList.toggle(clickedClass);
 }
 
 
-title.addEventListener("click",titleClick);
+h1.addEventListener("click",titleClick);
 // 1. element를 찾아라
 // 2. event를 listen
 // 3. event를 반응
