72586d696b8eb151b6b820499618d2e61f170442
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-08-31 22:01:45 +0900

    child->parent
    0831

--- softsquared/React/cloneairbnb/src/App.js
+++ softsquared/React/cloneairbnb/src/App.js
@@ -1,10 +1,31 @@
 import Header from './components/Header.js';
-import NavSlide from './components/NavSlide.js'
+import NavSlide from './components/NavSlide.js';
+import React,{useState} from 'react';
 function App() {
+  const [housetype,setHouseType] = useState("");
+
+  const HouseData = [
+    {
+      id:1,
+      houseType:"number1",
+      housename:"Kartepe",
+      country:"터키",
+      date:"9월 25일~30일",
+      distance:"7,892km",
+      price:128642,
+      
+    }
+  ]
+
+  const getData = (HouseType)=>{
+    setHouseType(HouseType);
+    console.log(`app.js number is ${housetype}`);
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <NavSlide></NavSlide>
+      <NavSlide housetype={housetype} getData={getData}></NavSlide>
     </div>
   );
 }
--- softsquared/React/cloneairbnb/src/components/NavSlide.js
+++ softsquared/React/cloneairbnb/src/components/NavSlide.js
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import Slider from 'react-slick';
 import "./slick.css";
 import "./slick-theme.css";
@@ -13,56 +14,67 @@ import Icon7 from '../img/icon/icon7.jpg'
 import Icon8 from '../img/icon/icon8.jpg'
 import Icon9 from '../img/icon/icon9.jpg'
 import Icon10 from '../img/icon/icon10.jpg'
-const NavSlide = ()=>{
+
+const NavSlide = ({getData})=>{
+    const [housenumber,setHouseNumber] = useState("number1");
+
+
     const settings={
-        dots:true,
+        dots:false,
         infinite:true,
         speed:500,
-        slidesToShow:5,
+        slidesToShow:7,
         slidesToScroll:2
     }
+
+    const onClickimage = (e)=>{
+        setHouseNumber(e.target.alt);
+        console.log(housenumber);
+        getData(housenumber);
+    }
+
 
     return(
         <div className="wrap-slider">
             <Slider {...settings}>
                 <div className="slider-block">
-                    <img src={Icon1}></img>
+                    <img src={Icon1} onClick={onClickimage} alt="number1"></img>
                     <span>A자형 주택</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon2}></img>
+                    <img src={Icon2} onClick={onClickimage} alt="number2"></img>
                     <span>섬</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon3}></img>
+                    <img src={Icon3} onClick={onClickimage} alt="number3"></img>
                     <span>해변 근처</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon4}></img>
+                    <img src={Icon4} onClick={onClickimage} alt="number4"></img>
                     <span>호숫가</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon5}></img>
+                    <img src={Icon5} onClick={onClickimage} alt="number5"></img>
                     <span>통나무집</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon6}></img>
+                    <img src={Icon6} onClick={onClickimage} alt="number6"></img>
                     <span>디자인</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon7}></img>
+                    <img src={Icon7} onClick={onClickimage} alt="number7"></img>
                     <span>초소형 주택</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon8}></img>
+                    <img src={Icon8} onClick={onClickimage} alt="number8"></img>
                     <span>국립공원</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon9}></img>
+                    <img src={Icon9} onClick={onClickimage} alt="number9"></img>
                     <span>기상천외한 숙소</span>
                 </div>
                 <div className="slider-block">
-                    <img src={Icon10}></img>
+                    <img src={Icon10} onClick={onClickimage} alt="number10"></img>
                     <span>캠핑장</span>
                 </div>
             </Slider>
